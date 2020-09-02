f379100473c94c7c58dd6afb758413bfe3cd60f1
Oğuz Ergül <erguloguz@hotmail.com>
2020-09-02 15:07:06 +0300

    Header ve Footer düzenlendi

---
+++ src/components/Footer.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="fixed-bottom">
+    <nav class="nav justify-content-center navbar-custom ">
+      <a class="navbar-brand navbar-custom-text">Made with 💖 in İstanbul</a>
+    </nav>
+  </div>
+</template>
+
+<script>
+export default {
+  name:"Footer"
+}
+</script>
+
+<style scoped>
+.navbar-custom{
+  display: flex;
+  background-color:#1B262C;
+  height: 60px;
+  align-items: center;
+  justify-content: center;
+  &-text{
+    color: white;
+    font-size: 22px;
+    font-family: saira;
+  }
+}
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,50 @@
+<template>
+
+  <nav class="navbar custom">
+    <Container>
+    <a class="custom-title" >ONE ONE TODO APP</a>
+
+    <router-link to="/add-new">
+      <div class="custom-add">
+        <a class="custom-add-text" href="">Add New Todo</a>
+      </div>
+    </router-link>
+    </Container>
+  </nav>
+
+</template>
+
+<script>
+import Container from "@/components/Container";
+export default {
+  name:"Header",
+  components: {Container}
+}
+</script>
+
+<style scoped>
+.custom{
+  background-color:#E7EAF2;
+  border-color: #1b262c;
+  &-add{
+    display: flex;
+    height: 40px;
+    width: 120px;
+    background-color: #2DCAD2;
+    align-items: center;
+    justify-content: center;
+    border-radius: 2px;
+    &-text {
+      font-size: 22px;
+      color: #1B262C;
+
+    }
+  }
+
+  &-title {
+    font-size: 28px;
+    color: #1B262C;
+
+  }
+}
+</style>
