c412d6db8e39298b858f621cc2c0ebeeb1f7b6c1
Oğuz Ergül <erguloguz@hotmail.com>
2020-09-02 15:10:31 +0300

    Vuex store ile state manangment yapıldı

--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,30 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import VuexPersistence from 'vuex-persist'
 
 Vue.use(Vuex)
 
+const vuexLocal = new VuexPersistence({
+  storage: window.localStorage,
+  key: 'library-app'
+})
+
+
 export default new Vuex.Store({
   state: {
+    todoList: [],
   },
   mutations: {
+    addTodoItem(state, payload) {
+      payload.index = state.todoList.length;
+      state.todoList.push(payload);
+    },
+    updateTodoItem(state, payload) {
+      state.todoList.splice(payload.index, 1, payload);
+    }
   },
-  actions: {
+  getters:{
+    getTodoList: (state) => state.todoList
   },
-  modules: {
-  }
+  plugins: [vuexLocal.plugin]
 })
